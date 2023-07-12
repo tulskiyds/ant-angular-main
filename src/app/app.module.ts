d20d01e6913faf987d950fdee20780aa96a6d60b
tulskiyds <tulskiy.ds@yandex.ru>
2023-07-12 17:07:08 +0300

    first_commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ru_RU } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { TableComponent } from './table/table.component';
+import { NgZorroAntdModule } from './ng-zorro-antd.module';
+
+registerLocaleData(ru);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NgZorroAntdModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: ru_RU }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
